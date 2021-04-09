7aa92cca3afec7133c39ea502a8b869ae174fac2
David <55385190+davidlashkhi@users.noreply.github.com>
2021-04-09 11:02:21 +0200

    add Deque, RandomizedQueue, Permutation

--- week2/Deque.java
+++ week2/Deque.java
@@ -1,125 +1,146 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-
 public class Deque<Item> implements Iterable<Item> {
 
-    private final Node head, tail;
-    private int size;
+    private Node head;
+    private Node tail;
+    private int size = 0;
+
+    private class Node {
+        Item item;
+        Node prev;
+        Node next;
+    }
 
     public Deque() {
-        this.size = 0;
-        this.head = new Node(null);
-        this.tail = new Node(null);
-        head.next = tail;
-        tail.prev = head;
     }
 
-    // is the deque empty?
     public boolean isEmpty() {
-        return size() == 0;
+        return size == 0;
     }
 
-    // return the number of items on the deque
     public int size() {
-        return this.size;
+        return size;
     }
 
-    // add the item to the front
     public void addFirst(Item item) {
         if (item == null) {
             throw new IllegalArgumentException();
         }
+
+        Node node = new Node();
+        node.item = item;
+
+        if (head == null) {
+            head = node;
+            tail = node;
+        }
+        else {
+            node.prev = head;
+            head.next = node;
+            head = node;
+        }
+
         size++;
-        Node temp = new Node(item);
-        temp.next = head.next;
-        temp.prev = head;
-        head.next.prev = temp;
-        head.next = temp;
     }
 
-    // add the item to the end
     public void addLast(Item item) {
         if (item == null) {
             throw new IllegalArgumentException();
         }
+
+        Node node = new Node();
+        node.item = item;
+
+        if (tail == null) {
+            tail = node;
+            head = node;
+        }
+        else {
+            node.next = tail;
+            tail.prev = node;
+            tail = node;
+        }
+
         size++;
-        Node temp = new Node(item);
-        temp.prev = tail.prev;
-        temp.next = tail;
-        tail.prev.next = temp;
-        tail.prev = temp;
     }
 
-    // remove and return the item from the front
     public Item removeFirst() {
-        if (isEmpty()) {
+        if (size == 0) {
             throw new NoSuchElementException();
         }
+
+        Item item = head.item;
+
+        if (size == 1) {
+            head = null;
+            tail = null;
+        }
+        else {
+            head = head.prev;
+            head.next = null;
+
+            if (size == 2) {
+                tail = head;
+            }
+        }
+
         size--;
-        Node temp = head.next;
-        temp.next.prev = head;
-        head.next = temp.next;
-        return temp.getItem();
+        return item;
     }
 
-    // remove and return the item from the end
     public Item removeLast() {
-        if (isEmpty()) {
+        if (size == 0) {
             throw new NoSuchElementException();
         }
-        size--;
-        Node temp = tail.prev;
-        temp.prev.next = tail;
-        tail.prev = temp.prev;
-        return temp.getItem();
 
+        Item item = tail.item;
+
+        if (size == 1) {
+            head = null;
+            tail = null;
+        }
+        else {
+            tail = tail.next;
+            tail.prev = null;
+
+            if (size == 2) {
+                head = tail;
+            }
+        }
+
+        size--;
+        return item;
     }
 
     public static void main(String[] args) {
         
     }
 
-    // return an iterator over items in order from front to end
     public Iterator<Item> iterator() {
-        return new ItemIterator();
+        return new DequeIterator();
     }
 
-    private class ItemIterator implements Iterator<Item> {
+    private class DequeIterator implements Iterator<Item> {
+        private Node current = head;
 
-        @Override
         public boolean hasNext() {
-            return !isEmpty();
+            return current != null;
         }
 
-        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
         public Item next() {
             if (!hasNext()) {
                 throw new NoSuchElementException();
             }
-            return removeFirst();
-        }
 
-        @Override
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
-    }
-
-    private class Node {
-
-        private final Item item;
-        private Node next;
-        private Node prev;
-
-        public Node(Item item) {
-            this.item = item;
-        }
-
-        public Item getItem() {
+            Item item = current.item;
+            current = current.prev;
             return item;
         }
-
     }
-
 }
